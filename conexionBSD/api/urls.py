1f96f7cf70eb8960506442753bb50f5b2355edc6
K3vinCC <kcortes2021@alu.uct.cl>
2024-09-05 18:23:43 -0400

    api

--- conexionBSD/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path,include
-from rest_framework import routers
-from api import views
-router=routers.DefaultRouter()
-router.register(r'/categoria',views.CategoriaViewSet)
-
-urlpatterns= [
-    path('',include(router.urls))
-]
