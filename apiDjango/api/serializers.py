ca1720c2f7bbe12286101d1fa88998be0dbfc282
ArminFH <afierro2021@alu.uct.cl>
2024-09-09 21:50:22 -0300

    agregada app armin a developer

--- apiDjango/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import CategoriaRev
-
-class CategoriaRevSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CategoriaRev
-        fields = '__all__'  # Incluye todos los campos del modelo
