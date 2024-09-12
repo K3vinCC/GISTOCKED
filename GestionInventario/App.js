e2a5e9c60fe8407b4c26c050299974c90081d040
Jonathan Huinca <jhuinca2020@alu.uct.cl>
2024-09-12 09:53:32 -0300

    Ruta de inicio sesión,para ingresar al Inicio

---
+++ GestionInventario/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Login from './src/screens/Login'
+
+import Home from './src/screens/Home';
+import Home1 from './src/screens/Home1'; // Assuming you'll create this for testing
+import Opciones from './src/screens/Opciones';
+import Ginventario from './src/screens/Ginventario'
+import { enableScreens } from 'react-native-screens';
+import 'react-native-safe-area-context';
+const Stack = createStackNavigator();
+enableScreens();
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Inicio Sesion">
+        <Stack.Screen name="Inicio sesion" component={Login}/>
+        <Stack.Screen name="Inicio" component={Home} />
+        <Stack.Screen name="Inicio1" component={Home1} />
+        <Stack.Screen name="Opciones" component={Opciones} options={{ headerStyle: {
+                                                                         backgroundColor: '#34495E'
+                                                                      },
+                                                                                 headerTintColor: '#fff',
+                                                                                 headerTitleStyle: {
+                                                                                   fontWeight: 'bold',}}}/>
+        <Stack.Screen name="inventario" component={Ginventario} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ GestionInventario/src/screens/Home1.js
@@ -0,0 +1,272 @@
+import React from "react";
+import { SafeAreaView, View, ScrollView, Image, Text, StyleSheet, } from "react-native";
+import { TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+export default (props) => {
+    const navigation = useNavigation();
+	return (
+		<SafeAreaView style={styles.container}>
+			<ScrollView  style={styles.scrollView}>
+				<View style={styles.row}>
+					<Image
+						source={require('../../assets/INICIO/menu.png')}
+						resizeMode = {"stretch"}
+						style={styles.image}
+					/>
+					<Text style={styles.text}>
+						{"Inicio"}
+					</Text>
+					<Image
+						source={require('../../assets/INICIO/notif.png')}
+						resizeMode = {"stretch"}
+						style={styles.image2}
+					/>
+				</View>
+				<View style={styles.row2}>
+					<View style={styles.column}>
+					    <TouchableOpacity onPress={() => navigation.navigate('inventario')}>
+                            <Image
+                                source={require('../../assets/INICIO/inventario.png')}
+                                resizeMode = {"stretch"}
+                                style={styles.image3}
+                            />
+                            <Text style={styles.text2}>
+                                {"Inventario"}
+                            </Text>
+                            <Text style={styles.text3}>
+                                {"Edita o inspecciona tu inventario"}
+                            </Text>
+                        </TouchableOpacity>
+					</View>
+					<View style={styles.column}>
+						<Image
+							source={require('../../assets/INICIO/venta.png')}
+							resizeMode = {"stretch"}
+							style={styles.image3}
+						/>
+						<Text style={styles.text4}>
+							{"Ventas"}
+						</Text>
+						<Text style={styles.text5}>
+							{"Genera una nueva\nventa de productos"}
+						</Text>
+					</View>
+				</View>
+				<View style={styles.row2}>
+					<View style={styles.column2}>
+						<Image
+							source={require('../../assets/INICIO/analisis.png')}
+							resizeMode = {"stretch"}
+							style={styles.image4}
+						/>
+						<Text style={styles.text6}>
+							{"Análisis de ventas"}
+						</Text>
+						<Text style={styles.text7}>
+							{"Inspecciona los\ngráficos de ventas de productos"}
+						</Text>
+					</View>
+					<View style={styles.column}>
+						<Image
+							source={require('../../assets/INICIO/user.png')}
+							resizeMode = {"stretch"}
+							style={styles.image3}
+						/>
+						<Text style={styles.text8}>
+							{"Usuarios"}
+						</Text>
+						<Text style={styles.text9}>
+							{"Añade, edita o\nelimina usuarios"}
+						</Text>
+					</View>
+				</View>
+				<View style={styles.column3}>
+					<Image
+						source={require('../../assets/INICIO/ajustes.png')}
+						resizeMode = {"stretch"}
+						style={styles.image3}
+					/>
+					<Text style={styles.text10}>
+						{"Ajustes"}
+					</Text>
+					<Text style={styles.text11}>
+						{"Configura tus\npreferencias de la\napp o tu usuario"}
+					</Text>
+				</View>
+				<View style={styles.row3}>
+					<Image
+						source = {{uri: "https://i.imgur.com/1tMFzp8.png"}}
+						resizeMode = {"stretch"}
+						style={styles.image5}
+					/>
+					<Image
+						source = {{uri: "https://i.imgur.com/1tMFzp8.png"}}
+						resizeMode = {"stretch"}
+						style={styles.image6}
+					/>
+					<Image
+						source = {{uri: "https://i.imgur.com/1tMFzp8.png"}}
+						resizeMode = {"stretch"}
+						style={styles.image7}
+					/>
+					<Image
+						source = {{uri: "https://i.imgur.com/1tMFzp8.png"}}
+						resizeMode = {"stretch"}
+						style={styles.image8}
+					/>
+				</View>
+			</ScrollView>
+		</SafeAreaView>
+	)
+}
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: "#FFFFFF",
+	},
+	column: {
+		width: 155,
+		backgroundColor: "#D8D8D8",
+		borderRadius: 20,
+		paddingTop: 22,
+		paddingBottom: 35,
+	},
+	column2: {
+		width: 155,
+		borderColor: "#5E27FD",
+		borderWidth: 1,
+		paddingVertical: 22,
+	},
+	column3: {
+		width: 155,
+		height: 180,
+		backgroundColor: "#D8D8D8",
+		borderRadius: 20,
+		paddingVertical: 22,
+		marginBottom: 34,
+		marginHorizontal: 25,
+	},
+	image: {
+		width: 18,
+		height: 16,
+	},
+	image2: {
+		width: 20,
+		height: 21,
+	},
+	image3: {
+		height: 41,
+		marginBottom: 14,
+		marginHorizontal: 57,
+	},
+	image4: {
+		height: 41,
+		marginBottom: 13,
+		marginHorizontal: 57,
+	},
+	image5: {
+		width: 20,
+		height: 18,
+	},
+	image6: {
+		width: 20,
+		height: 16,
+	},
+	image7: {
+		width: 18,
+		height: 18,
+	},
+	image8: {
+		width: 20,
+		height: 20,
+	},
+	row: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center",
+		marginBottom: 48,
+		marginHorizontal: 30,
+	},
+	row2: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center",
+		marginBottom: 25,
+		marginHorizontal: 23,
+	},
+	row3: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center",
+		marginHorizontal: 56,
+	},
+	scrollView: {
+		flex: 1,
+		backgroundColor: "#EDF1F1",
+		paddingVertical: 42,
+	},
+	text: {
+		color: "#1E1E1E",
+		fontSize: 20,
+	},
+	text2: {
+		color: "#000000",
+		fontSize: 20,
+		marginBottom: 20,
+		marginLeft: 33,
+	},
+	text3: {
+		color: "#000000",
+		fontSize: 14,
+		marginHorizontal: 17,
+		width: 121,
+	},
+	text4: {
+		color: "#000000",
+		fontSize: 20,
+		marginBottom: 20,
+		marginLeft: 47,
+	},
+	text5: {
+		color: "#000000",
+		fontSize: 14,
+		marginHorizontal: 18,
+		width: 119,
+	},
+	text6: {
+		color: "#000000",
+		fontSize: 20,
+		marginBottom: 1,
+		marginHorizontal: 29,
+	},
+	text7: {
+		color: "#000000",
+		fontSize: 14,
+		marginHorizontal: 20,
+		width: 113,
+	},
+	text8: {
+		color: "#000000",
+		fontSize: 20,
+		marginBottom: 20,
+		marginLeft: 38,
+	},
+	text9: {
+		color: "#000000",
+		fontSize: 14,
+		marginHorizontal: 26,
+		width: 103,
+	},
+	text10: {
+		color: "#000000",
+		fontSize: 20,
+		marginBottom: 20,
+		marginLeft: 44,
+	},
+	text11: {
+		color: "#000000",
+		fontSize: 14,
+		marginHorizontal: 22,
+		width: 111,
+	},
+});
