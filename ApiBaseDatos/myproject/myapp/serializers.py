bc14de711abe2c13ff89dfc2df049d0bdae4fb79
K3vinCC <kcortes2021@alu.uct.cl>
2024-10-18 00:20:10 -0300

    api

---
+++ ApiBaseDatos/myproject/myapp/serializers.py
@@ -0,0 +1,110 @@
+# from rest_framework import serializers
+# from .models import Categoria
+
+# class CategoriaSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Categoria
+#         fields = '__all__'  # Todos los campos
+
+
+from rest_framework import serializers
+from .models import (
+    AuthGroup, AuthGroupPermissions, AuthPermission, AuthUser, AuthUserGroups, AuthUserUserPermissions,
+    Categoria, DetalleVenta, DjangoAdminLog, DjangoContentType, DjangoMigrations, DjangoSession,
+    FormaPago, HistorialProductos, Inventario, RolUser, Usuario, Vendedores, Ventas
+)
+
+class AuthGroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AuthGroup
+        fields = '__all__'
+
+class AuthGroupPermissionsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AuthGroupPermissions
+        fields = '__all__'
+
+class AuthPermissionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AuthPermission
+        fields = '__all__'
+
+class AuthUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AuthUser
+        fields = '__all__'
+
+class AuthUserGroupsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AuthUserGroups
+        fields = '__all__'
+
+class AuthUserUserPermissionsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AuthUserUserPermissions
+        fields = '__all__'
+
+class CategoriaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Categoria
+        fields = '__all__'
+
+class DetalleVentaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DetalleVenta
+        fields = '__all__'
+
+class DjangoAdminLogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DjangoAdminLog
+        fields = '__all__'
+
+class DjangoContentTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DjangoContentType
+        fields = '__all__'
+
+class DjangoMigrationsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DjangoMigrations
+        fields = '__all__'
+
+class DjangoSessionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DjangoSession
+        fields = '__all__'
+
+class FormaPagoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FormaPago
+        fields = '__all__'
+
+class HistorialProductosSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = HistorialProductos
+        fields = '__all__'
+
+class InventarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Inventario
+        fields = '__all__'
+
+class RolUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RolUser
+        fields = '__all__'
+
+class UsuarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Usuario
+        fields = '__all__'
+
+class VendedoresSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vendedores
+        fields = '__all__'
+
+class VentasSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ventas
+        fields = '__all__'
